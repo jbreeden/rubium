06153f11bbbafa60e96fbc0e15f4dfd60461a8a0
jbreeden <jared.breeden@gmail.com>
2015-08-18 20:46:12 -0700

    Moving all editor code from JS to Ruby

--- samples/mruby-editor/MrubyEditor.rb
+++ samples/mruby-editor/MrubyEditor.rb
@@ -1,12 +1,71 @@
+JS.window[:MrubyEditor] = JS.function do |aceEditor, commandLine|
+  unless aceEditor && commandLine
+    raise "MrubyEditor expects two arguments - an ace editor, and a div with editable content for the command line"
+  end
+  MrubyEditor.new(aceEditor, commandLine)
+end
+
 class MrubyEditor
-  def initialize(aceEditor, commandLine = nil)
+  def initialize(aceEditor, commandLine)
     @ace = aceEditor
-    @document = @ace.getSession.doc
+    @session = @ace.getSession
+    @document = @session.doc
     @commandLine = commandLine
+
+    @ace.setOptions({
+      mode: "ace/mode/ruby",
+      tabSize: 2,
+      useSoftTabs: true
+    });
+
     bind_event_handlers
     if File.exists?("./MrubyEditorCommands.rb")
       self.instance_eval(File.read("./MrubyEditorCommands.rb"))
     end
+    @user_state = {}
+
+    use_editor_font_in_command_line
+    restore_editor_state
+  end
+
+  def use_editor_font_in_command_line
+    editor_style = JS.getComputedStyle(@ace.container)
+    @commandLine.style.fontFamily = editor_style.fontFamily;
+    @commandLine.style.fontSize = editor_style.fontSize
+  end
+
+  def save_editor_state
+    JS.localStorage.script = @ace.getValue
+    JS.localStorage.commandLine = @commandLine.textContent
+  end
+
+  def restore_editor_state
+    unless JS.localStorage.script.is_undefined?
+      @ace.setValue JS.localStorage.script
+    end
+
+    unless JS.localStorage.commandLine.is_undefined?
+      @commandLine.textContent = JS.localStorage.commandLine
+    end
+  end
+
+  def save_user_state
+    @user_state[:scroll_top] = @session.getScrollTop
+  end
+
+  def save_state
+    save_editor_state
+    save_user_state
+  end
+
+  def restore_user_state
+    @session.setScrollTop @user_state[:scroll_top] if @user_state[:scroll_top]
+  end
+
+  def maintain_user_state(&block)
+    save_state # editor & user states (so we can restore editor in case of crash)
+    block[]
+    restore_user_state
   end
 
   # Syntax sugar for adding commands
@@ -44,10 +103,36 @@ class MrubyEditor
         if e.keyCode.int_value == 13 # Enter
           e.preventDefault
           e.stopPropagation
+          save_editor_state
           self.instance_eval(@commandLine.textContent.to_s)
           if e.ctrlKey.bool_value
             @ace.focus
           end
+        end
+      end
+    end
+
+    command "runSelections", "Ctrl-R" do
+      maintain_user_state do
+        self.eval
+      end
+    end
+
+    command "runSelectionsAndPrint", "Ctrl-Shift-R" do
+      maintain_user_state do
+        results = self.eval
+        lines = []
+        @document.getAllLines.forEach do |line|
+          lines.push(line.to_s)
+        end
+        adjustment = 1
+        results.keys.each do |row_num|
+          result = results[row_num]
+          next unless result
+          result = result.to_s.gsub(/\r\n|\r/, "\n")
+          lines = result.split("\n")
+          @document.insertFullLines(row_num + adjustment, lines)
+          adjustment += lines.length
         end
       end
     end
--- samples/mruby-editor/MrubyEditorExt.rb
+++ samples/mruby-editor/MrubyEditorExt.rb
@@ -1,26 +1,18 @@
 class MrubyEditor
-  def replace(content)
-    content = content.to_s
-    @aceEditor.setValue(content)
-  end
-
-  def append(content)
-    @aceEditor.setValue(JS.create_string(@aceEditor.getValue.to_s + "\n" + content.to_s))
-  end
-
   def ofile(path)
     if File.exists?(path)
-      replace(File.read(path))
+      @ace.setValue(File.read(path))
     else
-      replace("File not found: #{path}")
+      @ace.setValue("File not found: #{path}")
     end
   end
 
   def sh(cmd, mode = :insert)
+    result = "`#{cmd}`\n#{`#{cmd}`}"
     if mode == :replace
-      replace "`#{cmd}`\n#{`#{cmd}`}"
+      @ace.setValue result
     else
-      insert "`#{cmd}`\n#{`#{cmd}`}"
+      each_cursor { @ace.insert result }
     end
   end
 end
